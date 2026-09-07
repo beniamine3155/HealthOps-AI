bf73930af6c761c4d4c780111f8ab8ea8f145f70
Beniamine Nahid <beniamine3155@gmail.com>
2026-09-07 20:48:07 +0600

    implemented model_loader functionality

--- api/routers/claim.py
+++ api/routers/claim.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+from api.schemas.claim_schema import ClaimPredictionRequest
 
 router = APIRouter()
 
 @router.post("/claim")
-def claim_status():
-    return {"message": "Claim"}
+def claim_status(request: ClaimPredictionRequest):
+    return {"message": "Claim", "input": request.model_dump()}
--- api/routers/risk.py
+++ api/routers/risk.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+from api.schemas.risk_schema import RiskPredictionRequest
 
 router = APIRouter()
 
 @router.post("/risk")
-def risk_score():
-    return {"message": "High"}
+def risk_score(request: RiskPredictionRequest):
+    return {
+            "message": "High", 
+            "input": request.model_dump()
+        }
