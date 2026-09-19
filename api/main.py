c2ec7a91cae920f25886de1acd6c4ad57e85def8
Beniamine Nahid <beniamine3155@gmail.com>
2026-09-19 09:34:10 +0600

    tested the schema, routes and artifacts

--- api/main.py
+++ api/main.py
@@ -20,9 +20,6 @@ app.include_router(claim.router, prefix="/predict", tags=["Claim Status Predicti
 app.include_router(monitoring.router, prefix="/monitor", tags=["Monitoring"])
 
 
-print("Registered routes:")
-for route in app.routes:
-    print(route.path)
 
 
 
